62e7866bc9912e9f98a223234b2217e080829696
Siguza <molt@gmx.net>
2013-03-23 22:19:38 +0100

    Changez

--- src/net/drgnome/waterproof/SweepAwayEvent.java
+++ src/net/drgnome/waterproof/SweepAwayEvent.java
@@ -9,46 +9,76 @@ import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.block.BlockEvent;
 
+/**
+ * A SweepAwayEvent is called whenever a water or lava block flows into a block and breaks it.
+ * WaterProof needs to be installed for this.
+ */
 public class SweepAwayEvent extends BlockEvent implements Cancellable
 {
     private static final HandlerList _handlers = new HandlerList();
     private final FluidType _fluid;
     private boolean _cancelled = false;
     
+    /**
+     * Instantiate a new SweepAwayEvent.
+     *
+     * @param block The affected Block.
+     * @param fluid The type of fluid that broke the block.
+     */
     public SweepAwayEvent(Block block, FluidType fluid)
     {
         super(block);
         _fluid = fluid;
     }
     
-    public FluidType getCause()
+    /**
+     * Get the fluid type which broke the block.
+     *
+     * @return The fluid type which broke the block.
+     */
+    public FluidType getFluidType()
     {
         return _fluid;
     }
     
+    /**
+     * {@inheritDoc}
+     */
     public boolean isCancelled()
     {
         return _cancelled;
     }
     
+    /**
+     * {@inheritDoc}
+     */
     public void setCancelled(boolean cancelled)
     {
         _cancelled = cancelled;
     }
     
-    public static enum FluidType
-    {
-        WATER,
-        LAVA;
-    }
-    
+    /**
+     * {@inheritDoc}
+     */
     public HandlerList getHandlers()
     {
         return _handlers;
     }
     
+    /**
+     * Used by Bukkit.
+     */
     public static HandlerList getHandlerList()
     {
         return _handlers;
+    }
+    
+    /**
+     * The types of fluids altered by WaterProof.
+     */
+    public static enum FluidType
+    {
+        WATER,
+        LAVA;
     }
 }
